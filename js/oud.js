b6ffc5c75cb8da126129d6bb7f5cafdf56ba7097
Emerson elias <151890972+emerson-elias@users.noreply.github.com>
2024-04-08 00:05:31 -0300

    1.1
    Foram adicionados a sessão "sobre mim" e sua responsividade, também foram feitas otimizações nos outros códigos e houve uma mudança na cor secundária

--- js/oud.js
+++ js/oud.js
@@ -13,15 +13,21 @@ li.map((elementos) => {
 })
 
 
+const know = document.getElementById('know')
+
+know.addEventListener('click',()=>{
+    console.log(know)
+})
+
+
 window.addEventListener("scroll", () => {
     const scroll_Y = window.scrollY
-    const header = document.getElementById('blur')
+    const header = document.getElementById('header')
     const navbar = document.getElementById('nav')
     const topo = document.getElementById('topo')
 
     navbar.classList.toggle('navbar_aux', scroll_Y > 100)
     header.classList.toggle('header_aux', scroll_Y > 150)
-
 
     if (scroll_Y > 250){
         topo.style.opacity = '1'
@@ -36,3 +42,13 @@ window.addEventListener("scroll", () => {
     }
 
 })
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
